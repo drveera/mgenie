a2e0033904a7017efcf1feaa2759994beb405929
Veera Manikandan Rajagopal <xrajagv01@node27-52.(none)>
2018-04-02 15:01:38 -0400

    gds subset update

--- modules/predixcan/train/gds.sub.R
+++ modules/predixcan/train/gds.sub.R
@@ -19,11 +19,11 @@ names(genes) <- c("gene","chr","start","end")
 genes$start <- genes$start - 1000000
 genes$end <- genes$end + 1000000
 genes.ranges <- with(genes, GRanges(seqnames = chr, IRanges(start=start,end=end), gene=gene))
-print(genes.ranges)
-print(snpannot)
 snpannot.sub <- subsetByOverlaps(snpannot,genes.ranges)
 saveRDS(snpannot.sub,outannot)
+print(head(snpannot.sub))
 index <- as.data.frame(snpannot.sub)$index
+print(range(index))
 
 ##subset gds
 gdsSubset(gds.file, outgds, sample.include = NULL,
