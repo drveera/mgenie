99a633d64aeb3ddc6d1c7593abbcd4a09d40dc53
veera_dr <drveeramanikandan@gmail.com>
2019-06-05 12:28:45 +0200

    overlap issue

--- modules/predixcan/qc-samples/pca.R
+++ modules/predixcan/qc-samples/pca.R
@@ -29,6 +29,9 @@ pca.eigen <- cbind(IID,pca.eigen)
 ##read referrence samples
 fam <- fread(reffam)
 refsamples = fam$V1
+##read bfile samples
+bfam <- fread(paste0(bfile,".fam"))
+bfamsamples <- bfam$V1
 
 pca.eigen$popgroup <- ifelse(pca.eigen$IID %in% refsamples,"Referrence","Cases")
 
@@ -46,15 +49,19 @@ pca.eigen$ell2sd6 <- (
 ) <=1
 ##pca.eigen$ell2sd6 = pca.eigen$ell2sd6*1
 
-pca.eigen$popgroup <- with(pca.eigen, ifelse(ell2sd6 & (!IID %in% refsamples),"keep",popgroup))
-pca.eigen$popgroup <- with(pca.eigen, ifelse((!ell2sd6) & (!IID %in% refsamples),"remove",popgroup))
+##pca.eigen$popgroup <- with(pca.eigen, ifelse(ell2sd6 & (!IID %in% refsamples),"keep",popgroup))
+##pca.eigen$popgroup <- with(pca.eigen, ifelse((!ell2sd6) & (!IID %in% refsamples),"remove",popgroup))
+
+pca.eigen$popgroup <- with(pca.eigen, ifelse(ell2sd6 & (IID %in% bfamsamples),"keep",popgroup))
+pca.eigen$popgroup <- with(pca.eigen, ifelse((!ell2sd6) & (IID %in% bfamsamples),"remove",popgroup))
 
 p2 <- ggplot(pca.eigen, aes(V1,V2, colour=popgroup)) + geom_point()
 plot2 <- paste0(outfile,".PCA.pdf")
 ggsave(plot2)
 
 fwrite(pca.eigen,paste0(outfile,".allsamples.mds"),sep="\t",na="NA")
-pca.eigen1 <- pca.eigen[ell2sd6==TRUE & popgroup!="Referrence",]
+##pca.eigen1 <- pca.eigen[ell2sd6==TRUE & popgroup!="Referrence",]
+pca.eigen1 <- pca.eigen[popgroup="keep",]
 
 ##repeat PCA
 pca2 <- snpgdsPCA(genofile,num.thread = 4,sample.id=pca.eigen1$IID)
@@ -67,6 +74,3 @@ fwrite(pca2.eigen,paste0(outfile,".mds"),sep="\t",na="NA")
 ##selected.fam <- data.table(IID=pca2.eigen$IID,FID=pca2.eigen$IID,PHENO=1)
 selected.fam <- fread(orginalfam)[V2 %in% pca2.eigen$IID]
 fwrite(selected.fam,paste0(outfile,".samples"),sep="\t",col.names = TRUE, na="NA",quote=FALSE)
-
-
-
