1b41d6e772c57b4cf407525d261ea0a00dbe03f2
Veera <drveeramanikandan@gmail.com>
2019-08-12 20:24:38 +0530

    Fread memory map error, alternative cut command

--- modules/predixcan/qc-dna/gentogds.R
+++ modules/predixcan/qc-dna/gentogds.R
@@ -16,7 +16,8 @@ sampledf$id <- paste(sampledf$ID_1,sampledf$ID_2)
 scandf <- data.frame(sampleID = sampledf$id, scanID = sampledf$ID_2, stringsAsFactors = FALSE)[-1,]
 
 ##chromosome
-bim <- fread(genfile,header=FALSE,select=1:2,colClasses = "numeric")
+##bim <- fread(genfile,header=FALSE,select=1:2,colClasses = "numeric")
+bim <- fread(paste0("cut -d ' ' -f 1,2 ", genfile), header=FALSE)
 names(bim) <- c("chr","rsid")
 chr <- bim$chr
 
